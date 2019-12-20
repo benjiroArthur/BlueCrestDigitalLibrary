f621278ce14d88c967f704b6bfcf15338ca9ae2e
benjiro <majorcaios@gmail.com>
2019-12-20 12:06:30 +0000

    open book

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -86,7 +86,9 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <a class="btn btn-success mx-2" href="{{route('download-book',[$book->id])}}"><span class="mdi mdi-download"></span> Download</a>
+              <!-- <a class="btn btn-success mx-2" href="{{route('download-book',[$book->id])}}"><span class="mdi mdi-download"></span> Download</a> -->
+
+                <a class="btn btn-success mx-2" href="{{$book->file_name}}"><span class="mdi mdi-download"></span> Download</a>
                 @if($review)
 
                     @if($review->like == true)
